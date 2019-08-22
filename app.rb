3d9acaac903698ad63e6f8eb8e6747693d1dd747
Keith Isham <kisham8899@gmail.com>
2019-08-22 00:14:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,11 +10,7 @@ class App < Sinatra::Base
       erb :index
     end
  
-    get "/info" do
-      "Testing the info page"
-    end
-    
-       get "/info" do
+     get "/info" do
       erb :info
     end
     
